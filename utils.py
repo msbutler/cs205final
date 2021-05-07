d45882081f024ac02b1dd2dbdda17689a4628061
Ubuntu <ubuntu@ip-172-31-21-72.ec2.internal>
2021-05-07 15:53:19 +0000

    update train test split

--- utils.py
+++ utils.py
@@ -26,46 +26,79 @@ def compress_im(root,basewidth):
             #print(os.path.getsize(file_path))
             #assert 0 ==1
             
-def train_test_split(n, flooded_img, nonflooded_img, unlabelled_img=np.array([])):
-    idxs = list(range(n))
-    s = int(np.floor(0.8*n)) # number of images for training
+
+""" splits labeled flooded data, labeled unflooded, and unlabelled data into training and test data
+
+if n = 0, then all data will be used; else, n samples from each data set will be selected for train-test split. 
+So if n>0, n must be min(len(flooded_img),len(non_flooded_img),len(unlabelled_img))
+
+assumes labelled data live in the data_img and that flooded appears before non-flooded 
+
+flooded_img: array of flooded images
+nonflooded_img: array of unflooded images
+unlabelled_img: array of unflooded images
+
+return
+
+train_idx: indices for data_img for training data
+test_idx: indices for data_img for test data
+train_labels: the labels associated with the training data
+test_labels: the labels associated with test data
+
+*optional return values if nonzero unlablled img is passed in*
+unlabel_train_idx: the indices for unlabelled_img for unlabelled training data
+unlabel_test_idx: the indices for unlabelled_img for unlabelled test data
+"""
+def train_test_split(flooded_img, nonflooded_img, unlabelled_img=np.array([]),n=0):
     
     train_idx = []
     test_idx = []
     
     #index range to select from (flooded img range, non flooded image range)
-    for s_i,e_i in [(0,len(flooded_img)),(len(flooded_img),len(nonflooded_img)+len(flooded_img))]:
+    for s_i,e_i in [(0,len(flooded_img)),(len(flooded_img),len(flooded_img)+len(nonflooded_img))]:
         
-        #print(s_i,e_i,n,s)
+        if n == 0:
+            n_s = e_i-s_i
+        else:
+            assert n <= e_i-s_i
+            n_s = n
+            
+        s = int(np.floor(0.8*n_s)) # number of images for training
         #get all poss indexes for set
         s_idx = list(range(s_i,e_i))
         random.shuffle(s_idx)
         
         train_idx.extend(s_idx[:s])      #first s images are for training
-        test_idx.extend(s_idx[s:n])      # next n-s images are for testing. 
-        #print(len(train_idx))
+        test_idx.extend(s_idx[s:n_s])      # next n-s images are for testing. 
+        print(f"Data len {e_i-s_i}; n is {n_s} ;train size {s}, test size {n_s-s}")
     
     train_idx = np.array(train_idx)
     test_idx = np.array(test_idx)
     train_idx.sort()
     test_idx.sort()
     
+    # data live in data_img, and flooded data appears first. 1 = flooded
     train_labels = [1 if x<len(flooded_img) else 0 for x in train_idx]
     test_labels = [1 if x<len(flooded_img) else 0 for x in test_idx]
 
-    print("Training Index: {}".format(train_idx))
-    print("Testing Index: {}".format(test_idx))
+    print(f"Training Indices len {len(train_idx)}")
+    print(f"Testing Indices len {len(test_idx)}")
+    
     if len(unlabelled_img)== 0:
         return train_idx, test_idx, train_labels, test_labels
     else:
-        
-        u_idx = list(range(n))
-        assert n < len(unlabelled_img)
+        if n ==0:
+            n_s = len(unlabelled_img)
+        else:
+            assert n <= len(unlabelled_img)
+            n_s = n
+            
+        u_idx = list(range(n_s))
         random.shuffle(u_idx)
         
         unlabel_train_idx = np.array(u_idx[:s])
-        unlabel_test_idx = np.array(u_idx[s:n])
+        unlabel_test_idx = np.array(u_idx[s:n_s])
         unlabel_train_idx.sort()
         unlabel_test_idx.sort()
-        print("Unlabeled Training Index: {}".format(unlabel_train_idx))
+        print(f"Unlabelled Data len {len(unlabelled_img)}; n is {n_s} ;train size {s}, test size {n_s-s}")
         return train_idx, test_idx, train_labels, test_labels, unlabel_train_idx, unlabel_test_idx
